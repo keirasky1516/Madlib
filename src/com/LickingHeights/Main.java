1a5672e1ba8ccb4b714dccf189dccf5f34255ee9
Keira <keira.plybon.2021@student.lhschools.org>
2018-09-07 08:17:28 -0400

    Initial commit of intellij

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,85 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+ //Declare
+Scanner keyboard;
+String greeting;
+String userName;
+String userPerson;
+String person;
+//initializing
+keyboard= new Scanner(System.in);
+greeting= "userName";
+person= "userPerson";
+
+
+
+
+System.out.println("Hello! Welcome to my Madlib.");
+userName= keyboard.nextLine();
+System.out.println(greeting + " " +userName);
+System.out.println("Give ma a person");
+userPerson= keyboard.nextLine();
+System.out.println(person + " " +userPerson);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*There once was a (person)
+who lived in (place).
+One day (he/she) decided to go (verb ending in ing),
+and found some (animal).
+It was (verb ending in ing) with ( animal 2).
+That is when (name of person) said (random word).
+After the (adjective) word was said both of the animals began to
+stare at the (profession) as if he was a (food 2).
+Once the (number) minutes of (adjective) staring was over,
+everyone ran until they got to a (noun). "Nobody touch the (random object)"
+(name of famous person) shouted. Just then a (mystical animal) came out of (place 2)
+and (action word) all over the (a group of people).
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
